c06b13d931cdab6b9db0ed791b82f8fea4a1bc15
Prince Raj <princeraj427@gmail.com>
2021-04-26 11:45:18 +0530

    My First Commit - App working fine.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # meeting-scheduler
 Design a meeting scheduler to book a meeting and return the name of the room booked.
+### [Problem Statement](/src/Problem+Statement.pdf)
---
+++ src/meeting_scheduler.cpp
@@ -0,0 +1,113 @@
+/*
+ * meeting_scheduler.cpp
+ *
+ *  Created on: 26-Apr-2021
+ *      Author: prince
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Meeting;
+
+typedef vector<Meeting> Calender;
+
+class Room {
+public:
+	Room(string);
+	bool book(int, int);
+	const string& getName() const;
+
+private:
+	string name;
+	Calender calender;
+};
+
+class Meeting {
+public:
+	Meeting(int, int, Room);
+	int getEnd() const;
+	int getStart() const;
+
+private:
+	int start,end;
+	Room room;
+};
+
+class Scheduler {
+	vector<Room> rooms;
+public:
+	Scheduler(vector<Room>);
+	string book(int, int);
+};
+
+
+Room::Room(string name){
+	this->name = name;
+}
+
+bool Room::book(int start,int end){
+	for(Meeting m: calender){
+		// to be implemented later
+		if(m.getStart() < end && start < m.getEnd())
+			return false;
+	}
+	Meeting meeting(start,end,*this);
+	calender.push_back(meeting);
+	return true;
+}
+
+const string& Room::getName() const {
+	return name;
+}
+
+int Meeting::getEnd() const {
+	return end;
+}
+
+int Meeting::getStart() const {
+	return start;
+}
+
+Meeting::Meeting(int start, int end, Room room) : room(room)
+{
+	this->start = start;
+	this->end = end;
+}
+
+Scheduler::Scheduler(vector<Room> rooms){
+	this->rooms = rooms;
+}
+
+string Scheduler::book(int start,int end){
+	for(Room &room: rooms){
+		bool flag = room.book(start,end);
+		if(flag==true)
+			return room.getName();
+	}
+	return "No rooms available.";
+}
+
+
+int main() {
+	Room room1("Atlas");
+	Room room2("Nexus");
+	Room room3("Hollycow");
+
+	vector<Room> roomVec;
+	roomVec.push_back(room1);
+	roomVec.push_back(room2);
+	roomVec.push_back(room3);
+
+	Scheduler scheduler(roomVec);
+
+	cout << scheduler.book(2,5) <<endl;
+	cout << scheduler.book(5,8) <<endl;
+	cout << scheduler.book(4,8) <<endl;
+	cout << scheduler.book(3,6) <<endl;
+	cout << scheduler.book(7,8) <<endl;
+	cout << scheduler.book(6,9) <<endl;
+	return 0;
+}
